db5a1e4e98fbe61c671fcf2ee406bf532c2b3b02
Thibaut CENS <Thibaut.CENS@edenred.com>
2026-01-09 13:24:53 +0100

    Initial commit

---
+++ dotfiles/.config/fish/conf.d/aliases-ls.fish
@@ -0,0 +1,5 @@
+alias l='eza -lh --icons=auto'
+alias ls='eza -1 --icons=auto'
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first'
+alias ld='eza -lhD --icons=auto'
+alias lt='eza --icons=auto --tree'
