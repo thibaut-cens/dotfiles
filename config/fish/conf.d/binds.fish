a8d09b9ced35e6cb0ba1448f08486a20f6fd0f6d
Thibaut CENS <Thibaut.CENS@edenred.com>
2026-01-12 10:58:45 +0100

    Fish bindings

---
+++ config/fish/conf.d/binds.fish
@@ -0,0 +1,9 @@
+bind alt-h backward-char-passive
+bind alt-j history-prefix-search-forward
+bind alt-k history-prefix-search-backward
+bind alt-l forward-char-passive
+
+bind shift-alt-h backward-word
+bind shift-alt-j history-token-search-forward
+bind shift-alt-k history-token-search-backward
+bind shift-alt-l forward-word
