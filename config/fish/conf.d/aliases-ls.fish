67d25a3e8573b6e2cd329db50b362aeda14f65ad
Thibaut Cens <thibaut.cens@gmail.com>
2026-01-13 15:18:57 +0100

    Stuff

--- config/fish/conf.d/aliases-ls.fish
+++ config/fish/conf.d/aliases-ls.fish
@@ -1,5 +1,13 @@
+# ==================== Eza ====================
 alias l='eza -lh --icons=auto'
 alias ls='eza -1 --icons=auto'
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first'
 alias ld='eza -lhD --icons=auto'
 alias lt='eza --icons=auto --tree'
+
+# ==================== Git ====================
+alias g='git'
+
+# ==================== Gtrash ====================
+alias gm='gtrash put' # gtrash move (easy to change to rm)
+alias rm='echo -e "If you want to use rm really, then use $(tput bold)gm$(tput sgr0) instead." && false'
