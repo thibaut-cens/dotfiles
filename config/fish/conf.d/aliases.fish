e9230ae98d9db0b2ada083fcd2f8a1d8e33dcbd8
Thibaut Cens <thibaut.cens@gmail.com>
2026-01-16 16:20:23 +0100

    Update fish files

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -7,7 +7,20 @@ alias lt='eza --icons=auto --tree'
 
 # ==================== Git ====================
 alias g='git'
+alias ga='git add'
+alias gps='git push'
+alias gpl='git pull'
+alias gcmsg='git commit --message'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+alias gbr='git checkout --branch'
+alias gst='git status'
+alias glog='git log --oneline --decorate'
 
 # ==================== Gtrash ====================
 alias gm='gtrash put' # gtrash move (easy to change to rm)
 alias rm='echo -e "If you want to use rm really, then use $(tput bold)gm$(tput sgr0) instead." && false'
+
+# ==================== Editor ====================
+alias vim="nvim"
